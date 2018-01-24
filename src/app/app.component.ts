ecdb408a2895fc89b05e752a78bb6a109912ad5d
Marla Foreman <marlaforeman@marlas-mbp.lan>
2018-01-24 14:45:17 -0600

    Creating space array for 1 row of board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,14 @@ import { Space }	 from './space';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  	private row: Space[] = [];
+
+  	ngOnInit() {
+  		for (var i = 0; i < 8; i = i+2) {
+  			this.row[i] = new Space(i, 0, true);
+  			this.row[i+1] = new Space(i+1, 0, false);
+  		}
+  	}
+
 }
