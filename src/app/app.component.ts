241e1d42c516696e4627332a28ea5c13612a9e7f
Marla Foreman <marlaforeman@Marlas-MacBook-Pro.local>
2018-01-27 11:29:21 -0500

    Populating board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,25 @@ import { Space }	 from './space';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  	private row: Space[] = [];
+  	//private row: Space[] = [];
+  	private board = new Array();
 
   	ngOnInit() {
   		for (var i = 0; i < 8; i = i+2) {
-  			this.row[i] = new Space(i, 0, true);
-  			this.row[i+1] = new Space(i+1, 0, false);
+  			let rowEven = new Array();
+  			let rowOdd = new Array();
+  			for (var j = 0; j < 8; j = j+2) {
+  				rowEven[j] = new Space(0, j, true);
+  				rowEven[j+1] = new Space(0, j+1, false);
+  			}
+  			for (var j = 0; j < 8; j = j+2) {
+  				rowOdd[j] = new Space(0, j, false);
+  				rowOdd[j+1] = new Space(0, j+1, true);
+  			}
+  			this.board[i] = rowEven;
+  			this.board[i+1] = rowOdd;
   		}
+  		console.log(this.board);
   	}
 
 }
