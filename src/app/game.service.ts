4f9e5f8d08c16365189984104aaab793c205b6e3
Marla Foreman <marlaforeman@marlas-mbp.lan>
2018-02-06 19:46:56 -0600

    Added findPiece and placePiece functions to game service

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -1,5 +1,6 @@
 import { Injectable }     from '@angular/core';
 import { Piece, Pawn }	  from './piece';
+import { Space }          from './space';
 import { CheckerBoard }	  from './checkerBoard';
 
 @Injectable()
@@ -14,27 +15,44 @@ export class GameService {
     resetGame() {
         this.board = new CheckerBoard().board;
         for (let i = 0; i < 3; i++) {
-          for (let j = 0; j < 8; j++) {
-            if (this.board[i][j].playable === true) {
-              this.board[i][j].addPiece(new Pawn('red', i, j));
+            for (let j = 0; j < 8; j++) {
+                if (this.board[i][j].playable === true) {
+                  this.board[i][j].addPiece(new Pawn('red', i, j));
+                }
             }
-          }
         }
         for (let i = 5; i < 8; i++) {
-          for (let j = 0; j < 8; j++) {
-            if (this.board[i][j].playable === true) {
-              this.board[i][j].addPiece(new Pawn('black', i, j));
+            for (let j = 0; j < 8; j++) {
+                if (this.board[i][j].playable === true) {
+                  this.board[i][j].addPiece(new Pawn('black', i, j));
+                }
             }
-          }
         }
+    }
+
+    // Finds a piece on the board and returns the space it is on
+    findPiece(p: Piece): Space {
+        let sp: Space = null;
+        this.board.forEach(row => row.forEach(space => {
+            if (space.piece === p) {
+                sp = space;
+            }
+        }));
+        return sp;
+    }
+
+    // Place piece on board according to coordinates on the piece
+    placePiece(p: Piece) {
+        let row = p.row;
+        let col = p.col;
     }
 
   	// Clicking a piece on the board causes (only) that piece to be selected
   	selectAPiece(p: Piece) {
-  		this.clearSelectedPiece();
-  		if (p != null ) {
-  			p.selected = true;
-  		}
+    		this.clearSelectedPiece();
+    		if (p != null ) {
+      			p.selected = true;
+    		}
   	}
 
     // Clear the selected flag from the board so no piece is selected
