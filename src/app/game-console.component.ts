0ae79be620bbffe0f8c1f8a3a2a14e686eb21256
Marla Foreman <marlaforeman@Marlas-MacBook-Pro.local>
2018-02-12 11:37:14 -0600

    Shell setup of game-console; injecting gameService into game-console

--- src/app/game-console.component.ts
+++ src/app/game-console.component.ts
@@ -1,5 +1,6 @@
 // This will be on the side of the actual checkers game board, showing things like the reset button, whose turn it is, and whether someone has won the game or not
 import { Component }	from '@angular/core';
+import { GameService }	from './game.service';
 
 @Component({
   selector: 'game-console',
@@ -8,4 +9,8 @@ import { Component }	from '@angular/core';
 })
 export class GameConsoleComponent {
 	
+	constructor(
+	  	private service: GameService
+	) {}
+
 }
