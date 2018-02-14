341a4cf9f3399a8a0f81ab53fb832e6fcb7d5da8
Marla Foreman <marlaforeman@marlas-mbp.lan>
2018-02-14 13:23:48 -0600

    Hooked up isWinner and added resetGame behavior subject
    Bug because when you reset the game from the app component it doesn’t
    reset the game console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } 	         from '@angular/core';
+import { OnInit } 		         from '@angular/core';
 import { GameBoardComponent }    from './game-board.component';
 import { GameConsoleComponent }  from './game-console.component';
 import { GameService }	   		 from './game.service';
+import { Observable }      		 from 'rxjs/Observable';
+import { BehaviorSubject }       from 'rxjs/BehaviorSubject';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +14,26 @@ import { GameService }	   		 from './game.service';
 export class AppComponent {
 	isWinner = false;
 
+	// Observables
+    public isWinner$: Observable<boolean>;
+
+    // Behavior Subjects
+	public _resetGame: BehaviorSubject<boolean>;
+
 	constructor(
   		  private service: GameService
   	) {}
 
+  	ngOnInit() {
+  		// Observables
+  		this.isWinner$ = this.service.isWinnerObs;
+
+  		// Behavior Subjects
+		this._resetGame = this.service.resetGameBeh;
+  	}
+
 	onReset() {
-		this.service.resetGame();
+		this._resetGame.next(true);
 	}
 
 }
