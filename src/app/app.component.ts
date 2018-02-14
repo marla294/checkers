b1adf18b9cd3cd8b74a3a41f9bb9eabe5fe749fe
Marla Foreman <marlaforeman@marlas-mbp.lan>
2018-02-14 12:13:48 -0600

    Hooking up reset game button
    Just don’t look at it… need to style ;)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } 	         from '@angular/core';
 import { GameBoardComponent }    from './game-board.component';
 import { GameConsoleComponent }  from './game-console.component';
 import { GameService }	   		 from './game.service';
-import { Observable }      		 from 'rxjs/Observable';
-
 
 @Component({
   selector: 'app-root',
@@ -13,10 +11,12 @@ import { Observable }      		 from 'rxjs/Observable';
 export class AppComponent {
 	isWinner = false;
 
-	// Observables
-    public resetGame$: Observable<boolean>;
-
 	constructor(
   		  private service: GameService
   	) {}
+
+	onReset() {
+		this.service.resetGame();
+	}
+
 }
