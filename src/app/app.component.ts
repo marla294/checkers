5cb250115bc21400f7b5eef7115af85723db57be
Marla Foreman <marlaforeman@Marlas-MacBook-Pro.local>
2018-02-15 09:47:54 -0600

    Fixing up the winner notification screen on app component
    Still doesn’t say who won (always says red), will fix that soon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { BehaviorSubject }       from 'rxjs/BehaviorSubject';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-	isWinner = false;
+	isWinner = true;
 
 	// Observables
     public isWinner$: Observable<boolean>;
